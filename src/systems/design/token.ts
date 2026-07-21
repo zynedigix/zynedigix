be9f7b569a4356fed1b02733f9ec0ca4ad9cc1ba
ZyneDigix <zynedigix@gmail.com>
2026-07-21 20:56:16 +0530

    Initial Project of ZyneDigix Foundation

---
+++ src/systems/design/token.ts
@@ -0,0 +1,15 @@
+export const colors = {};
+
+export const spacing = {};
+
+export const radius = {};
+
+export const typography = {};
+
+export const shadows = {};
+
+export const motion = {};
+
+export const breakpoints = {};
+
+export const zIndex = {};
